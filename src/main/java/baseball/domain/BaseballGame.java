20f556481642bdb8625da87adafbd21a595ee5f5
lakey001 <suyounlake@naver.com>
2022-04-14 22:48:55 +0900

    refactor: 랜덤 수 생성시 Randoms.pickNumberInRange() 활용

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -2,6 +2,7 @@ package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
@@ -11,8 +12,24 @@ public class BaseballGame {
     private int ballCounter;
 
     public BaseballGame() {
-        this.randomNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        this.randomNumbers = generateRandomNumbers();
         this.gameEnd = false;
+    }
+
+    private List<Integer> generateRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            randomNumbers.add(generateUniqueRandomNumber(randomNumbers));
+        }
+        return randomNumbers;
+    }
+
+    private int generateUniqueRandomNumber(List<Integer> randomNumbers) {
+        int random = Randoms.pickNumberInRange(1, 9);
+        while (randomNumbers.contains(random)) {
+            random = Randoms.pickNumberInRange(1, 9);
+        }
+        return random;
     }
 
     public List<Integer> getRandomNumbers() {
